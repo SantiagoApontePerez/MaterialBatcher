fdb5da3f2bcd43090fcc5fa3e2c4c8fe98b3b7b7
SantiagoApontePerez <Lamleecs@outlook.com>
2025-07-11 12:10:52 +0100

    Stable ver.

--- MaterialBatcher/Scripts/Editor/CreateMaterialsFromTextures.cs
+++ MaterialBatcher/Scripts/Editor/CreateMaterialsFromTextures.cs
@@ -1,15 +1,16 @@
-﻿using UnityEngine;
+﻿using System.IO;
 using UnityEditor;
+using UnityEngine;
 
-namespace MaterialsBatcher
+namespace MaterialBatcher.MaterialBatcher.MaterialBatcher.Scripts.Editor
 {
 	public class CreateMaterialsFromTextures : EditorWindow
 	{
 		#region Private Fields
 
-		private DefaultAsset textureFolder;
-		private string targetFolderPath = "Assets/";
-		private Shader shader;
+		private DefaultAsset _textureFolder;
+		private string _targetFolderPath = "Assets/";
+		private Shader _shader;
 
 		#endregion
 
@@ -22,13 +23,13 @@ namespace MaterialsBatcher
     	private void OnGUI()
 	    {
 		    EditorGUILayout.LabelField("Source Textures:", EditorStyles.boldLabel);
-		    textureFolder = (DefaultAsset)EditorGUILayout.ObjectField("Texture Folder", textureFolder, typeof(DefaultAsset), false);
+		    _textureFolder = (DefaultAsset)EditorGUILayout.ObjectField("Texture Folder", _textureFolder, typeof(DefaultAsset), false);
 
 		    EditorGUILayout.Space();
 
 		    EditorGUILayout.LabelField("Target Directory:", EditorStyles.boldLabel);
 		    EditorGUILayout.BeginHorizontal();
-		    targetFolderPath = EditorGUILayout.TextField(targetFolderPath);
+		    _targetFolderPath = EditorGUILayout.TextField(_targetFolderPath);
 		    if (GUILayout.Button("...", GUILayout.Width(24)))
 		    {
 			    var picked = EditorUtility.OpenFolderPanel("Select Target Folder", Application.dataPath, "");
@@ -37,7 +38,7 @@ namespace MaterialsBatcher
 			    {
 				    if (picked.StartsWith(Application.dataPath))
 				    {
-					    targetFolderPath = "Assets" + picked.SubString(Application.dataPath.Length);
+					    _targetFolderPath = "Assets" + picked.Substring(Application.dataPath.Length);
 				    }
 				    else
 				    {
@@ -50,19 +51,19 @@ namespace MaterialsBatcher
 		    EditorGUILayout.Space();
 
 		    EditorGUILayout.LabelField("Material Settings", EditorStyles.boldLabel);
-		    shader = (Shader)EditorGUILayout.ObjectField("Base Shader", shader, typeof(Shader), false);
+		    _shader = (Shader)EditorGUILayout.ObjectField("Base Shader", _shader, typeof(Shader), false);
 
 		    EditorGUILayout.Space();
 
 		    if (GUILayout.Button("Batch to Material"))
 		    {
-			    if (sourceFolder == null)
+			    if (_textureFolder == null)
 			    {
 				    EditorUtility.DisplayDialog("Error", "Please assign a source texture folder first.", "OK");
 				    return;
 			    }
 			    
-			    if (string.IsNullOrEmpty(targetFolderPath))
+			    if (string.IsNullOrEmpty(_targetFolderPath))
 			    {
 				    EditorUtility.DisplayDialog("Error", "Please select a valid target directory.", "OK");
 				    return;
@@ -74,11 +75,11 @@ namespace MaterialsBatcher
     	private void Generate()
     	{
 		    // Ensure output folder exists
-		    if (!AssetDatabase.IsValidFolder(targetFolderPath))
-			    AssetDatabase.CreateFolder(Path.GetDirectoryName(targetFolderPath), Path.GetFileName(targetFolderPath));
+		    if (!AssetDatabase.IsValidFolder(_targetFolderPath))
+			    AssetDatabase.CreateFolder(Path.GetDirectoryName(_targetFolderPath), Path.GetFileName(_targetFolderPath));
 
 		    // Get all textures in source folder
-		    var sourcePath = AssetDatabase.GetAssetPath(textureFolder);
+		    var sourcePath = AssetDatabase.GetAssetPath(_textureFolder);
 		    var guids = AssetDatabase.FindAssets("t:Texture2D", new[] { sourcePath });
 
 		    foreach (var guid in guids)
@@ -87,18 +88,18 @@ namespace MaterialsBatcher
 			    var tex = AssetDatabase.LoadAssetAtPath<Texture2D>(texPath);
 
 			    // Create material
-			    var mat = new Material(shader ?? Shader.Find("Standard"));
+			    var mat = new Material(_shader ?? Shader.Find("Standard"));
 			    mat.mainTexture = tex;
 
 			    // Save material asset
 			    var matName = Path.GetFileNameWithoutExtension(texPath) + ".mat";
-			    var savePath = Path.Combine(targetFolderPath, matName);
+			    var savePath = Path.Combine(_targetFolderPath, matName);
 			    AssetDatabase.CreateAsset(mat, savePath);
 		    }
 
 		    AssetDatabase.SaveAssets();
 		    AssetDatabase.Refresh();
-		    EditorUtility.DisplayDialog("Done", "Materials generated in:\n" + targetFolderPath, "OK");
+		    EditorUtility.DisplayDialog("Done", "Materials generated in:\n" + _targetFolderPath, "OK");
     	}
 	}
 }
